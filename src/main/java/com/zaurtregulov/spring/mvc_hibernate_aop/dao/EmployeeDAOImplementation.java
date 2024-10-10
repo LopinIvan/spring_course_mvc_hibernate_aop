6d63796eea8bec82984173e107df90d25b0b5c83
Ivan <xsardesxsardes@gmail.com>
2024-10-10 16:06:04 +0800

    second commit 1.Создание entity класса Employee(класс представляющий собой таблицу в базе данных). 2.Создание интерфейса DAO и класса DAO имплементирующего этот интерфейс. Отвечает за выполнение CRUD операций для сущностей Employee. 3.Разделяем (бизнес-логику) и (логику доступа к данным).

---
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/dao/EmployeeDAOImplementation.java
@@ -0,0 +1,32 @@
+package com.zaurtregulov.spring.mvc_hibernate_aop.dao;
+
+import com.zaurtregulov.spring.mvc_hibernate_aop.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImplementation implements EmployeeDAO {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public EmployeeDAOImplementation(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+
+        Session session = sessionFactory.getCurrentSession();
+        List<Employee> allEmployees = session.createQuery("from Employee"
+                , Employee.class).getResultList();
+
+        return allEmployees;
+    }
+}
---
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/entity/Employee.java
@@ -0,0 +1,86 @@
+package com.zaurtregulov.spring.mvc_hibernate_aop.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "emp_id")
+    private int id;
+
+    @Column(name = "emp_name")
+    private String firstName;
+
+    @Column(name = "emp_surname")
+    private String lastName;
+
+    @Column(name = "emp_department")
+    private String department;
+
+    @Column(name = "emp_salary")
+    private int salary;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, int salary, String department, String lastName) {
+        this.firstName = firstName;
+        this.salary = salary;
+        this.department = department;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}

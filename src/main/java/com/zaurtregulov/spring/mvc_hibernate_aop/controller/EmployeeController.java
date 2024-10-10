557837c5d6b03f6748216170d81d9793e048706b
Ivan <xsardesxsardes@gmail.com>
2024-10-10 16:50:59 +0800

    third commit 1.Создание контроллера EmployeeController для получения с помощью DAO сущностей Employees из базы данных и отображения их во view. 2.Обеспечиваем связь между доступом к данным (DAO) и шаблонами (view).

---
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.zaurtregulov.spring.mvc_hibernate_aop.controller;
+
+import com.zaurtregulov.spring.mvc_hibernate_aop.dao.EmployeeDAO;
+import com.zaurtregulov.spring.mvc_hibernate_aop.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class EmployeeController {
+
+    private final EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @RequestMapping("/")
+    public String showAllEmployees(Model model) {
+
+        List<Employee> allEmployees = employeeDAO.getAllEmployees();
+        model.addAttribute("allEmps", allEmployees);
+
+        return "all-employees";
+    }
+
+}
